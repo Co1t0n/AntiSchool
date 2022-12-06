4269f6004e2b94b38e86ade4281ba56a284cbd21
Co1t0n <92403275+Co1t0n@users.noreply.github.com>
2022-12-06 10:28:12 -0500

    Create games.js

---
+++ pages/games.js
@@ -0,0 +1,11 @@
+export const games = [
+    {
+        gamename: "Retro Bowl",
+        gameIMG: "images/rb.jpg",
+        link: "project-lulo.github.io/games/retro-bowl/",
+        viewed_on: 0,
+        genre: "Sports"
+    },
+    
+    },
+]   
