9b21a8c4b985dd52143bb6192401072c52d1e8ef
Co1t0n <92403275+Co1t0n@users.noreply.github.com>
2023-01-11 08:32:12 -0500

    Delete IframePopup.js

--- IframePopup.js
+++
@@ -1,1 +0,0 @@
-javascript:var frame = document.createElement('iframe'); frame.src="[LINK HERE]"; frame.style.position="fixed"; frame.style.top="0%"; frame.style.right="0%";frame.style.height="100%";frame.style.width="100%"; frame.style.zIndex="100000"; document.body.appendChild(frame);
